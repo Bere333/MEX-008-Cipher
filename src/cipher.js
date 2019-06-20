f8af48b737f3d1cae78bc617f70e2e3b690d9adc
Brenda Berenice Ramos Hernández <elcosmossuena@gmail.com>
2019-06-20 06:00:55 -0500

    El decifrado y el cifrado pasaron los test

--- src/cipher.js
+++ src/cipher.js
@@ -1,9 +1,7 @@
 window.cipher = {
-  encode: (offset, string) => {
-    let offset2 = parseInt(offset);//Convertir el string de offset1(porque es un string) a numero
+  encode: (offset, string) => {//aqui se llaman los parametros que definieron en el index.js, por eso no afecta que cambien los nombres(los llama).
+    let offset2 = parseInt(offset);//Convertir el string de offset1(porque es un string) a numero para que pueda ser usado en la formula
     //console.log(typeof offset2) se uso para ver el tipo de dato
-
-    //let lengthMessage = string.length;
      let letters = "";//caja vacía para guardar los strings que se generen en el bucle for
      let indexM = 0;//indice con el que se inicia(queremos que comience al principio de las palabras introducidas)
      for (let i = 0; i < string.length; i = i + 1){//queremos que cumpla la condicion de que, al empezar con el indice de la primer letra, i sea menor que la longitud de la palabra introducida
@@ -27,7 +25,29 @@ window.cipher = {
        //console.log(letters); se uso para ver todas las letras
     }
     return letters;
-    }
-    };
-
-    
+  }, 
+decode: (offset, string) => {
+  let offset3 = parseInt(offset);//Convertir el string de offset1(porque es un string) a numero para que pueda ser usado en la formula
+  //console.log(typeof offset2) se uso para ver el tipo de dato
+   let letters1 = "";//caja vacía para guardar los strings que se generen en el bucle for
+   //let indexM1 = 0;//indice con el que se inicia(queremos que comience al principio de las palabras introducidas)
+   for (let i = 0; i < string.length; i = i + 1){//queremos que cumpla la condicion de que, al empezar con el indice de la primer letra, i sea menor que la longitud de la palabra introducida
+     let charAtMessage1 = string.charAt(i);
+     //console.log(charAtMessage);
+     let indexAscii1 = string.indexOf(charAtMessage1);
+     //console.log(indexAscii);
+     let asciiMessage1 = string.charCodeAt(indexAscii1);//número ASCII del primer carácter del mensaje introducido. Aumenta en uno con cada ciclo for terminado
+     //console.log(asciiMessage);
+     let asciiLetter1 = (asciiMessage1 - 90 - offset3)%26 + 90;//Obtiene el número de ASCII del indice dado
+     //console.log(asciiLetter);
+     let letterCipher1 = String.fromCharCode(asciiLetter1);//Dado el numero ASCII devuelve la letra correspondiente en ASCII
+     //console.log(letterCipher);
+     letters1 = letters1 + letterCipher1;
+     //console.log(letters);
+     //document.getElementById("cipher1").value = letters;//mostrar valor en el input con id=cipher1
+     //console.log("hola");
+     //indexM1 = indexM1 + 1;
+   }
+   return letters1;
+}
+};
--- src/index.js
+++ src/index.js
@@ -57,52 +57,36 @@ document.getElementById("boton7").addEventListener("click", () => {
   //console.log("boton7");
   let ocultarDesencriptar = document.getElementById("instruccionesE");
   ocultarDesencriptar.style.display = "none";
-  let regresarDesencriptar = document.getElementById("encriptar")
+  let regresarDesencriptar = document.getElementById("encriptar");
   regresarDesencriptar.style.display = "block";
 })
 
 
+document.getElementById("boton2").addEventListener("click", () => {
+  //console.log("boton7");
+  let ocultarDesencriptar = document.getElementById("instruccionesE");
+  ocultarDesencriptar.style.display = "none";
+  let regresarDesencriptar = document.getElementById("encriptar");
+  regresarDesencriptar.style.display = "none";
+  let mostrarDesencriptar = document.getElementById("desencriptar");
+  mostrarDesencriptar.style.display ="block";
+})
 
 
-//console.log('boton6');
-document.getElementById("boton6").addEventListener("click", () => {
-let message = document.getElementById("paraEncriptar").value; //"hola"
-//contenido del mensaje introducido
+document.getElementById("boton6").addEventListener("click", () => {//se manda llamar el boton
+let message = document.getElementById("paraEncriptar").value; //contenido del mensaje introducido
 //console.log(message);
-let offset1= document.getElementById("offset").value; //3
-//let lengthMessage = message.length;
-
-//window.cipher.encode(offset,message) //krod
-
-let encode1 = window.cipher.encode(offset1, message);//llama al objeto encode
+let offset1= document.getElementById("offset").value; //Manda a llamar al offset introducido(el offset no es un numero es una string)
+let encode1 = window.cipher.encode(offset1, message);//llama a la propiedad encode (a su vez encode es una funcion metodo) del onjeto window.cipher con estos parametros
 document.getElementById("cipher1").value = encode1;//utilizo el objeto anterior para mostrar el codigo en el input
 
-
-//valor de offset o número fijo en la fórmula del código ASCII
-//console.log(offset1);
-//let lengthMessage = message.length;//longitud de mensaje introducido
-//console.log(lengthMessage);
 });
-/*let letters = " ";//caja vacía para guardar los strings que se generen en el bucle for
-let indexM = 0;//indice con el que se inicia(queremos que comience al principio de las palabras introducidas)
-for (let i = 0; i < lengthMessage; i = i + 1){//queremos que cumpla la condicion de que, al empezar con el indice de la primer letra, i sea menor que la longitud de la palabra introducida
-  const charAtMessage = message.charAt(indexM);
-  //console.log(charAtMessage);
-  let indexAscii = message.indexOf(charAtMessage);
-  //console.log(indexAscii);
-  let asciiMessage = message.charCodeAt(indexAscii);//número ASCII del primer carácter del mensaje introducido. Aumenta en uno con cada ciclo for terminado
-  //console.log(asciiMessage);
-  const asciiLetter = (asciiMessage - 65 + offset1) % 26 + 65;//Obtiene el número de ASCII del indice dado
-  //console.log(asciiLetter);
-  const letterCipher = String.fromCharCode(asciiLetter);//Dado el numero ASCII devuelve la letra correspondiente en ASCII
-  //console.log(letterCipher);
-  letters = letters + letterCipher;
-  //console.log(letters);
-  document.getElementById("cipher1").value = letters;//mostrar valor en el input con id=cipher1
-  //console.log("hola");
-  indexM = indexM + 1;
-  //console.log(indexM);
-}
 
-  });
-  */
+document.getElementById("botonDesencriptar").addEventListener("click", () => {//se manda llamar el boton
+let message1 = document.getElementById("paraDesencriptar").value; //contenido del mensaje introducido
+//console.log(message);
+let offset2= document.getElementById("offsetDes1").value; //Manda a llamar al offset introducido(el offset no es un numero es una string)
+let decode1 = window.cipher.decode(offset2, message1);//llama a la propiedad encode (a su vez encode es una funcion metodo) del onjeto window.cipher con estos parametros
+document.getElementById("cipher3").value = decode1;//utilizo el objeto anterior para mostrar el codigo en el input
+
+});
